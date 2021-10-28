6b460332f506219ad4143c5f972588a08230ad31
YeshPatel23 <dipci00004019@student.maseno.ac.ke>
2021-10-28 10:58:53 +0300

    initialize music events page

---
+++ pages/events/add.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default function Addpage() {
+  return (
+    <div>
+      <Layout title="Add Event">
+        <h1>Add Events</h1>
+      </Layout>
+    </div>
+  );
+}
---
+++ pages/events/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default function Eventspage() {
+  return (
+    <div>
+      <Layout title="Events">
+        <h1>My Events</h1>
+      </Layout>
+    </div>
+  );
+}
